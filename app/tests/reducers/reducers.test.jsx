87fecebd2b11fa28a472e30d88e338c680380eb8
Daniel Sanjuán <angeldaniel.sanjuan@dev.bbva.com>
2017-11-04 23:09:09 +0100

    Refactor app to work with localstorage

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -60,5 +60,25 @@ describe('Reducers', () =>{
       expect(res[0].completed).toEqual(false);
       expect(res[0].completedAt).toEqual(undefined);
     });
+
+    it ('should add existing todos', () => {
+      var todos = [{
+        id: '123',
+        text: 'Something',
+        completed: true,
+        completedAt: 125,
+        createdAt: 123
+      }];
+
+      var action = {
+        type: 'ADD_TODOS',
+        todos
+      };
+
+      var res = reducers.todosReducer(df([]),df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0]).toEqual(todos[0]);
+    });
   });
 });
